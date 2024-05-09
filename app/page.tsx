b55f0f8a5cae74508439a9723cd2a0cf41070e26
jahid28 <jahidrhps123@gmail.com>
2024-05-10 01:24:27 +0530

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,6 @@ import { useEffect, useRef, useState, ChangeEvent } from "react";
 // import { getCookie } from 'cookies-next';
 import ClipLoader from "react-spinners/ClipLoader";
 import PostBox from "@/components/PostBox";
-// import { OpenAI } from 'openai'
 
 export default function Home() {
 
@@ -20,71 +19,14 @@ export default function Home() {
   }, []);
 
   
-  const [imageSrc, setImageSrc] = useState<string | null>(null);
 
-  const [prompt, setPrompt] = useState('');
-  const [response, setResponse] = useState('');
-
-  // const handleSubmit = async (e:any) => {
-  //   e.preventDefault();
-
-  //   try {
-  //     const url = 'https://api.openai.com/v1/completions';
-
-  //     const aidata = {
-  //       model: 'text-davinci-003', // Choose the desired ChatGPT model
-  //       prompt: prompt,
-  //       max_tokens: 1024, // Adjust for desired response length
-  //       temperature: 0.7, // Controls randomness (0: deterministic, 1: creative)
-  //     };
-
-  //     const options = {
-  //       method: 'POST',
-  //       headers: {
-  //         Authorization: `Bearer ${API_KEY}`,
-  //         'Content-Type': 'application/json',
-  //       },
-  //       body: JSON.stringify(aidata),
-  //     };
-
-  //     const response = await fetch(url, options);
-  //     const data = await response.json();
-  //     setResponse(data.choices[0].text.trim()); // Access response text
-  //   } catch (error) {
-  //     console.error(error);
-  //     setResponse('Error occurred. Please try again later.');
-  //   } finally {
-  //     setPrompt(''); // Clear prompt after submission
-  //   }
-  // };
-
-
-
-// openai.chat.completions.create({ 
-//     model: "gpt-3.5-turbo",
-//     messages: [
-//         { role: "user", content: "Hello ChatGPT" }
-//     ]
-// }).then(res => {
-//     console.log(res)
-//     res.choices.forEach( out => console.log(out.message) );
-// });
   
 
   return (
     <>
     <div className="mt-20">
 
-      {/* <form onSubmit={handleSubmit}>
-        <textarea
-          value={prompt}
-          onChange={(e) => setPrompt(e.target.value)}
-          placeholder="Enter your prompt here..."
-        />
-        <button type="submit">Send</button>
-      </form>
-      {response && <p>ChatGPT Response: {response}</p>} */}
-   
+    
     </div>
      <PostBox/>
       <div className="bg-color text-color mt-28">{c}</div>
